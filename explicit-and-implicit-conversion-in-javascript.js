67737179b9d439927e871bc71fb00b9c1ff5a10f
Zee Gr8dy <kanis_grady@icloud.com>
2025-03-25 14:03:00 -0500

    Debugged part 1 of code and gave examples of type conversion for part 2 of code

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -17,16 +17,38 @@ Include at least one edge case, like NaN, undefined, or null .
 Use console.log() to clearly show the before-and-after type conversions.
 
 */
+//Part 1: Debugging Challenge
+// explicit type conversation Number() needs to be used
+console.log("This is Part 1!")
+let result = Number("5") - 2; //using the number() conversion explicitly converts 5 into a number before subtraction
+console.log("The result is: " + result); //output is 3
 
-
-let result = "5" - 2;
-console.log("The result is: " + result);
-
-let isValid = Boolean("false");
+//Boolean conversion of non-empty string
+let isValid = Boolean("false" === "true");//explicitly comparing to avoid a "false" Truth value
 if (isValid) {
     console.log("This is valid!");
+} else {
+  console.log ("This is not valid!"); // output is "This is not valid"
 }
 
+//make sure variable age is treated as a number for the operation
 let age = "25";
-let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+let totalAge = Number(age) + 5; //using the number() conversion explicitly converts 25 into a number before addition
+console.log("Total Age: " + totalAge); //output is 30
+
+
+
+          //Part 2: Write Your Own Examples
+
+          console.log("This is Part 2!")
+          let implicitEx = "19" - 20; //javascript automatically defines "19" as a number
+          console.log(implicitEx); //output is "-1"
+          console.log(typeof implicitEx + " is the output."); //output is "number"
+
+          let explicitEX = "19" + "20";
+          console.log(explicitEX);
+          console.log(typeof explicitEX + " is the output before."); //output is string
+
+          let convertedNum = Number(explicitEX); //convert string to number
+          console.log(convertedNum); //output is 1920
+          console.log(typeof convertedNum + " is the output after."); //output is number
